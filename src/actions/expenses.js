876d0d86148b11586907297b04d2a0e1145fb7a3
Omar Samir <spacemountainrock@gmail.com>
2020-02-18 00:30:45 +0200

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -39,6 +39,17 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return dispatch => {
+    return db
+      .ref(`expenses/${id}`)
+      .remove()
+      .then(() => {
+        dispatch(removeExpense({ id }));
+      });
+  };
+};
+
 export const editExpense = (id, updates) => ({
   type: "EDIT_EXPENSE",
   id,
