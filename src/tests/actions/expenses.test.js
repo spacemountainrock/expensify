5fce839a439c9d02e5452e0a4de0d65e7229b20a
Omar Samir <spacemountainrock@gmail.com>
2020-02-18 00:56:12 +0200

    Add startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
   removeExpense,
   setExpenses,
   startSetExpenses,
-  startRemoveExpense
+  startRemoveExpense,
+  startEditExpense
 } from "../../actions/expenses";
 import expenses from "../fixtures/expenses";
 import db from "../../firebase/firebase";
@@ -51,13 +52,34 @@ test("should remove expense from firebase", done => {
     });
 });
 
-test("should set up edit expense action", () => {
+test("should set up edit expense action object", () => {
   const action = editExpense("121212", { note: "New Note Value" });
   expect(action).toEqual({
     type: "EDIT_EXPENSE",
     id: "121212",
     updates: { note: "New Note Value" }
   });
+});
+
+test("should edit expense from firebase", done => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = { amount: 21.45 };
+  store
+    .dispatch(startEditExpense(id, updates))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: "EDIT_EXPENSE",
+        id,
+        updates
+      });
+      return db.ref(`expenses/${id}`).once("value");
+    })
+    .then(snapshot => {
+      expect(snapshot.val().amount).toBe(updates.amount);
+      done();
+    });
 });
 
 test("should setup add expense action with provided values", () => {
